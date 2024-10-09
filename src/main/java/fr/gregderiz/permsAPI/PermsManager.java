a39346b336e21f70e536a67cfb954b2b4be34a5d
BuildTools <unconfigured@null.spigotmc.org>
2024-10-09 12:49:34 +0200

    Transformed player permissions to a list

--- src/main/java/fr/gregderiz/permsAPI/PermsManager.java
+++ src/main/java/fr/gregderiz/permsAPI/PermsManager.java
@@ -1,41 +1,48 @@
 package fr.gregderiz.permsAPI;
 
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 public final class PermsManager {
-    private final Map<UUID, String> permissions;
+    private final Map<UUID, Set<String>> permissions;
 
     public PermsManager() {
         this.permissions = Maps.newHashMap();
     }
 
+    public boolean isPlayerRegistered(Player player) {
+        return this.permissions.containsKey(player.getUniqueId());
+    }
+
     public boolean hasPermission(Player player, String permission) {
-        if (!this.permissions.containsKey(player.getUniqueId())) return false;
-        return this.permissions.get(player.getUniqueId()).equalsIgnoreCase(permission);
+        return this.permissions.get(player.getUniqueId()).contains(permission);
     }
 
     public void addPermission(Player player, String permission) {
+        if (!this.isPlayerRegistered(player)) this.permissions.put(player.getUniqueId(), Sets.newHashSet());
         if (hasPermission(player, permission)) return;
 
-        this.permissions.put(player.getUniqueId(), permission);
+        this.permissions.get(player.getUniqueId()).add(permission);
     }
 
     public void removePermission(Player player, String permission) {
+        if (!this.isPlayerRegistered(player)) return;
         if (!hasPermission(player, permission)) return;
 
-        this.permissions.remove(player.getUniqueId(), permission);
+        this.permissions.get(player.getUniqueId()).remove(permission);
     }
 
     public void removePermissions(Player player) {
         this.permissions.remove(player.getUniqueId());
     }
 
-    public Map<UUID, String> getPermissions() {
+    public Map<UUID, Set<String>> getPermissions() {
         return this.permissions;
     }
 
