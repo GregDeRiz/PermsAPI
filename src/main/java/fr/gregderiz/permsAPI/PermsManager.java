42f4345772f685ad221ffc6b940667fb218dc3ab
BuildTools <unconfigured@null.spigotmc.org>
2024-10-09 12:15:36 +0200

    Creating a new permission manager

--- src/main/java/fr/gregderiz/permsAPI/PermsManager.java
+++ src/main/java/fr/gregderiz/permsAPI/PermsManager.java
@@ -1,77 +1,42 @@
 package fr.gregderiz.permsAPI;
 
 import com.google.common.collect.Maps;
-import com.google.common.collect.Sets;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.plugin.Plugin;
 
 import java.util.Map;
-import java.util.Set;
 import java.util.UUID;
 
 public final class PermsManager {
-    private final Plugin plugin;
-    private final Map<UUID, PermissionAttachment> permissions;
+    private final Map<UUID, String> permissions;
 
-    public PermsManager(Plugin plugin) {
-        this.plugin = plugin;
+    public PermsManager() {
         this.permissions = Maps.newHashMap();
     }
 
-    public boolean isRegistered(Player player) {
-        return this.permissions.containsKey(player.getUniqueId());
-    }
-
-    public void register(Player player) {
-        if (isRegistered(player)) return;
-
-        PermissionAttachment attachment = player.addAttachment(this.plugin);
-        this.permissions.put(player.getUniqueId(), attachment);
-    }
-
-    public void unRegister(Player player) {
-        if (!isRegistered(player)) return;
-
-        PermissionAttachment attachment = this.permissions.get(player.getUniqueId());
-        removePermissions(player);
-        player.removeAttachment(attachment);
-        this.permissions.remove(player.getUniqueId());
-    }
-
     public boolean hasPermission(Player player, String permission) {
-        return getAttachment(player).getPermissible().hasPermission(permission);
+        if (!this.permissions.containsKey(player.getUniqueId())) return false;
+        return this.permissions.get(player.getUniqueId()).equalsIgnoreCase(permission);
     }
 
     public void addPermission(Player player, String permission) {
         if (hasPermission(player, permission)) return;
 
-        getAttachment(player).setPermission(permission, true);
+        this.permissions.put(player.getUniqueId(), permission);
     }
 
     public void removePermission(Player player, String permission) {
         if (!hasPermission(player, permission)) return;
 
-        getAttachment(player).unsetPermission(permission);
+        this.permissions.remove(player.getUniqueId(), permission);
     }
 
     public void removePermissions(Player player) {
-        Set<PermissionAttachmentInfo> permissionInfos = Sets.newHashSet(player.getEffectivePermissions());
-        for (PermissionAttachmentInfo permissionInfo : permissionInfos) {
-            String permission = permissionInfo.getPermission();
-            getAttachment(player).unsetPermission(permission);
-        }
+        this.permissions.remove(player.getUniqueId());
     }
 
-    public Map<UUID, PermissionAttachment> getPermissions() {
+    public Map<UUID, String> getPermissions() {
         return this.permissions;
-    }
-
-    public PermissionAttachment getAttachment(Player player) {
-        if (!isRegistered(player)) register(player);
-        return this.permissions.get(player.getUniqueId());
     }
 
     public void destroy() {
@@ -79,7 +44,7 @@ public final class PermsManager {
             Player player = Bukkit.getPlayer(uuid);
             if (player == null || !player.isOnline()) continue;
 
-            unRegister(player);
+            removePermissions(player);
         }
     }
 }
--- src/main/java/fr/gregderiz/permsAPI/PermsPlugin.java
+++ src/main/java/fr/gregderiz/permsAPI/PermsPlugin.java
@@ -1,23 +1,7 @@
 package fr.gregderiz.permsAPI;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PermsPlugin extends JavaPlugin {
-    private PermsManager permsManager;
 
-    @Override
-    public void onDisable() {
-        if (this.permsManager != null) this.permsManager.destroy();
-    }
-
-    public PermsPlugin register() {
-        this.permsManager = new PermsManager(this);
-        Bukkit.getPluginManager().registerEvents(new PermsListener(this.permsManager), this);
-        return this;
-    }
-
-    public PermsManager getPermsManager() {
-        return this.permsManager;
-    }
 }
